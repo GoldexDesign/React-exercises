179cb0d171ecb8cd7f0c5a61ffc91c2e52dc7272
GoldexDesign <zlajavictory@gmail.com>
2022-12-28 13:31:36 +0100

    expenseItem

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,9 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Zlatko has been started!</h2>
       <h2>And its working!</h2>
-      <p>this is also visible</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
