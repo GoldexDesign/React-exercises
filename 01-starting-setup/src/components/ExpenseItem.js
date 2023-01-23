c8618f1287e7bdfb94fbdad4eaf5b927fe5f5ece
GoldexDesign <zlajavictory@gmail.com>
2023-01-23 17:23:00 +0100

    21.01.23

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,4 +1,14 @@
-function ExpenseItem() {
-  return <h2>Expense item! </h2>;
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 export default ExpenseItem;
